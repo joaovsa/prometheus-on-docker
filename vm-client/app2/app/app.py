689f56e206bfc7439ee1bc69246077fb0ce49a44
João Vitor <jvsms13@gmail.com>
2019-07-01 19:31:22 -0300

    correctly receiving prometheus API response and creating dicts

--- vm-client/app2/app/app.py
+++ vm-client/app2/app/app.py
@@ -1,21 +1,50 @@
 from typing import List, Dict
 from flask import Flask
-import mysql.connector
+#import mysql.connector
 import requests
 import json
 
+SERVICES = ['app2_app_1', 'app2_db_1', 'cadvisor', 'node-exporter', 'prometheus']
+
 app = Flask(__name__)
 
-def request_prom():
+def request_cpu(vec_dicts):
+    #fills container name, container ID and cpu %
     response = requests.get('http://192.168.50.2:9090/api/v1/query',
+         params={'query' : "rate(container_cpu_usage_seconds_total[5m]) * 100"})
+    resp_dict = response.json()
+
+    #for each result fills the dict with curresponding name and ID.
+    #ignores nom named SERVICES for class example simplicity
+    for resp in resp_dict['data']['result']:
+        try:
+            if resp['metric']['name'] in SERVICES:
+                d = {}
+                d['cont_id'] = resp['metric']['id']
+                d['cont_name'] = resp['metric']['name']
+                d['cpu_name'] = resp['metric']['cpu']
+                d['cpu_usage'] = resp['value'][1]
+                vec_dicts.append(d)
+        except:
+            pass 
+    
+    
+
+def request_prom():
+    insertions = []    
+    """response = requests.get('http://192.168.50.2:9090/api/v1/query',
          params={'query' : "container_memory_working_set_bytes{name='node-exporter'}"})
     dictio = response.json()
     for val in dictio['data']['result']:
         print('id: {} value: {}'.format(val['metric']['id'], val['value'][0]))
     result = response.json()
-    return result
+    return result"""
+    request_cpu(insertions)
+    #debug purps
+    for ins in insertions:
+        print("id: {} name: {} cpu: {}".format(ins['cont_id'], ins['cont_name'], ins['cpu_usage']))
 
-def cadvisordb() -> List[Dict]:
+""" def cadvisordb() -> List[Dict]:
     config = {
         'user': 'root',
         'password': 'root',
@@ -40,7 +69,7 @@ def cadvisordb() -> List[Dict]:
     cursor.close()
     connection.close()
 
-    return results
+    return results """
 
 
 @app.route('/')
@@ -48,7 +77,7 @@ def index() -> str:
     #get jsons from prometheus server
     return json.dumps({'request' : request_prom()})
     #dump mysql
-    return json.dumps({'cadvisor': cadvisordb()})
+    #return json.dumps({'cadvisor': cadvisordb()})
 
 
 if __name__ == '__main__':
