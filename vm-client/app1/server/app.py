a634bcc56e72d471d68694bd94547fc9283f7259
Luciane <luciane.silva@msn.com>
2019-06-30 21:25:08 -0300

    Adding client and server apps

---
+++ vm-client/app1/server/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request, send_file
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './files/'
+
+@app.route('/', methods=['GET'])
+def get():
+  try:
+    return send_file('tiny-puppy.jpg', attachment_filename='cute-dog.jpg')
+  except Exception as e:
+    return str(e)
+
+@app.route('/', methods=['POST'])
+def post():
+  try:
+    f = request.files['files']
+    f.save(f.filename)
+    return jsonify({'status': 'Yay! You just posted a file.'})
+  except Exception as e:
+    return str(e)
+
+if __name__ == "__main__":
+  app.run(debug=True, host='0.0.0.0', port=5000)
