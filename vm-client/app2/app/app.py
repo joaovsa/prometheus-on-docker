9c45c250a759bb5b8d52e089a4231a27382eb02b
João Vitor <jvsms13@gmail.com>
2019-07-01 23:40:31 -0300

    aaaa

--- vm-client/app2/app/app.py
+++ vm-client/app2/app/app.py
@@ -117,13 +117,11 @@ def cadvisordb(insertions) -> List[Dict]:
     cursor = connection.cursor(prepared=true)
 
 
-    sql = "INSERT INTO prometheus (cont_id, cont_name, cpu_name, cpu_usage, mem_usage, bytes_rx, bytes_tx) VALUES (%s, %s,%s, %s,%s, %s,%s)"
+    sql = """INSERT INTO prometheus (cont_id, cont_name, cpu_name, cpu_usage, mem_usage, bytes_rx, bytes_tx) VALUES (%s, %s, %s, %s, %s, %s, %s)"""
     val = [('dummy-container', 'cont_name', 'cpu_name', '47.212', '22.2', '123', '321'),
     ('dummy-container', 'cont_name', 'cpu_name', '47.212', '22.2', '123', '321'),
     ('dummy-container', 'cont_name', 'cpu_name', '47.212', '22.2', '123', '321')]
-    """ for ins in insertions:
-        val.append(tuple(ins.values()))
-     """
+     
     r = cursor.executemany(sql, val)
     connection.commit()
     print(cursor.rowcount, "was inserted.") 
